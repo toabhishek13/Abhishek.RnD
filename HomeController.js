f194509ebff5e0ac64b0bab2400c5c4bab597728
Abhishek Kumar <toabhishek13@hotmail.com>
2017-11-16 11:11:02 +0530

    Add files via upload

---
+++ HomeController.js
@@ -0,0 +1,20 @@
+﻿
+(function (homeController) {
+    var data=require("../data");
+    homeController.init = function (app)
+    {
+        app.get("/", function (req, res) {
+
+            // res.send("<html><body><h1>Hi There</h1></body></html>");
+            //res.render("ejs/index", { title : "Express + EJS"});
+
+            //  res.render("index", { title: "Express + vash" });
+
+            data.getNoteCategories(function (err, results) {
+
+                res.render("index", { msg: "Hi there Abhishek", title: "Node Work" ,  error: err, categories: results });
+
+            });         
+        });
+     };
+   })(module.exports);
